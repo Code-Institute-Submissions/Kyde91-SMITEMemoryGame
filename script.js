545ecef51f68546f87ba8f0ae66db8809918870e
Kyde <kmc91@hotmail.com>
2024-01-01 18:49:54 +0000

    added game rules modal and style

--- script.js
+++ script.js
@@ -103,3 +103,22 @@ function restart() {
   gridContainer.innerHTML = "";
   generateCards();
 }
+
+// Modal for rules of the game and how to play
+var modal = document.getElementById("modal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
