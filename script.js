9e1d8999569ed8c9c9ab0939dd98e474f4dc7ae2
Kyde <kmc91@hotmail.com>
2024-01-03 13:54:21 +0000

    updated to show alt text for SEO

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function generateCards() {
     cardElement.classList.add("card");
     cardElement.setAttribute("data-name", card.name);
     cardElement.innerHTML = `<div class="front">
-    <img class="front-image" src="${card.image}"/>
+    <img class="front-image" src="${card.image}" alt="${card.name}"/>
     </div>
     <div class="back"></div>`;
     gridContainer.appendChild(cardElement);
